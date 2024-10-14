645b3c25b1c7f19361d703977f472f55c2ca223f
cheesecrust <jack0810@kookmin.ac.kr>
2024-10-14 18:06:18 +0900

    INTERNAL: Refactor node method

--- node_util.go
+++ node_util.go
@@ -17,7 +17,24 @@ func init_node(node *node, port *string, client_ports []string) {
 	node.client_cnt = len(client_ports)
 	node.status = Follower
 	node.is_voted = false
-	node.election_timeout = time.Duration(150+rand.Intn(150)) * time.Millisecond
 	node.heartbeat_channel = make(chan bool)
+	reset_timeout(node)
+}
 
+func change_status(node *node, status int) {
+	node.mu.Lock()
+	node.status = status
+	if status == Follower {
+		node.is_voted = false
+		reset_timeout(node)
+	} else if status == Candidate {
+		node.is_voted = true
+	}
+	node.mu.Unlock()
+}
+
+func reset_timeout(node *node) {
+	node.mu.Lock()
+	node.election_timeout = time.Duration(150+rand.Intn(150)) * time.Millisecond
+	node.mu.Unlock()
 }
